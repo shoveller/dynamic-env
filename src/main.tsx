d98b6c57c3790dde6ffefe470196b3229de49be9
cinos81 <cinos81@gmail.com>
2025-04-15 22:13:17 +0900

    React Router 추가 및 .env 파일을 .gitignore에 포함시킴

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router'
+
+const router = createBrowserRouter(createRoutesFromElements(<Route path='/' element={<App />} />), { basename: 'dynamic-env'})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
